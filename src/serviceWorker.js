0b5f48698671b4b371b667c621ed2eb8944a7abb
elchanan89 <42324050+elchanan89@users.noreply.github.com>
2018-10-21 13:24:05 +0300

    ver 0.1 components

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -12,12 +12,12 @@
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === '[::1]' ||
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  )
 );
 
 export function register(config) {
@@ -43,7 +43,7 @@ export function register(config) {
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit http://bit.ly/CRA-PWA'
+            'worker. To learn more, visit http://bit.ly/CRA-PWA'
           );
         });
       } else {
@@ -68,7 +68,7 @@ function registerValidSW(swUrl, config) {
               // content until all client tabs are closed.
               console.log(
                 'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See http://bit.ly/CRA-PWA.'
+                'tabs for this page are closed. See http://bit.ly/CRA-PWA.'
               );
 
               // Execute callback
@@ -128,4 +128,29 @@ export function unregister() {
       registration.unregister();
     });
   }
+}
+
+// SendGrid API service
+const sgMail = require('@sendgrid/mail');
+
+export function sendEmail(data) {
+  sgMail.setApiKey('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');
+  const msg = {
+    to: data['Mail Address'],
+    from: 'test@example.com',
+    subject: 'New Lead',
+    html: 'First Name: ' + data['First Name'] + '<br>' + 
+      'Last Name: ' + data['Last Name'] + '<br>' + 
+      'Mail Address: ' + data['Mail Address'] + '<br>' + 
+      'Phone Number: ' + data['Phone Number']
+  };
+  
+  sgMail.client.defaultRequest.headers = {
+    "allow-http-request-headers-from-headers": "*",
+    "site-control-permitted-cross-domain-policies": "all",
+    "allow-access-from-domain": "*",
+    "allow-access-from-secure": "false"
+}
+  
+  sgMail.send(msg);
 }
